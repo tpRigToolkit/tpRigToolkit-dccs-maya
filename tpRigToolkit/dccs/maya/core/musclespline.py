15d7fe0c8544926d0321d6c6e789b5136d9da44d
Tomas Poveda <tpovedatd@gmail.com>
2020-09-10 04:24:01 +0200

    Keep adding features to core Muscle Spline implementation

--- tpRigToolkit/dccs/maya/core/musclespline.py
+++ tpRigToolkit/dccs/maya/core/musclespline.py
@@ -23,24 +23,25 @@ class MuscleSpline(object):
 
         self._name = kwargs.pop('name', 'Char01_Spine')
         self._size = kwargs.pop('size', 1.0)
-        self._num_controls = kwargs.pop('num_controls', 3)
+        self._insertion_controls = kwargs.pop('insertion_controls', 3)
         self._control_type = kwargs.pop('control_type', 'cube')
-        self._num_drivens = kwargs.pop('num_drivens', 5)
+        self._driven_joints = kwargs.pop('driven_joints', 5)
         self._driven_type = kwargs.pop('driven_type', 'joint')
-        self._constrain_mid = kwargs.pop('constrain_mid', False)
-        self._lock_scale = kwargs.pop('lock_scale', True)
+        self._constraint_mid_controls = kwargs.pop('constraint_mid_controls', False)
+        self._lock_controls_scale = kwargs.pop('lock_scale', True)
         self._lock_jiggle_attributes = kwargs.pop('lock_jiggle_attributes', False)
-        self._ctrl_suffix = kwargs.pop('control_suffix', 'ctrl')
-        self._jnt_suffix = kwargs.pop('joint_suffix', 'jnt')
-        self._grp_suffix = kwargs.pop('group_suffix', 'grp')
+        self._control_suffix = kwargs.pop('control_suffix', 'ctrl')
+        self._joint_suffix = kwargs.pop('joint_suffix', 'jnt')
+        self._group_suffix = kwargs.pop('group_suffix', 'grp')
+        self._create_sets = kwargs.pop('create_sets', True)
         self._driven_suffix = kwargs.pop('driven_suffix', 'drv')
-        self._main_set_name = kwargs.pop('main_set_name', 'setMUSCLERIGS')
-        self._rig_set_suffix = kwargs.pop('rig_set_suffix', 'RIG')
+        self._main_muscle_set_name = kwargs.pop('main_muscle_set_name', 'setMUSCLERIGS')
+        self._muscle_set_suffix = kwargs.pop('muslce_set_suffix', 'RIG')
         self._muscle_spline_name = kwargs.pop('muscle_spline_name', 'muscleSpline')
         self._controls_group_suffix = kwargs.pop('controls_group_suffix', 'controls')
         self._joints_group_suffix = kwargs.pop('joints_group_suffix', 'joints')
-        self._root_suffix = kwargs.pop('root_suffix', 'root')
-        self._auto_suffix = kwargs.pop('auto_suffix', 'auto')
+        self._root_group_suffix = kwargs.pop('root_group_suffix', 'root')
+        self._auto_group_suffix = kwargs.pop('auto_group_suffix', 'auto')
 
         self._main_group = None
         self._spline_node = None
@@ -48,7 +49,36 @@ class MuscleSpline(object):
         self._controls = list()
         self._constraint_groups = list()
         self._root_groups = list()
+        self._drivens_group = None
         self._drivens = list()
+
+    @property
+    def main_group(self):
+        return self._main_group
+
+    @property
+    def spline_node(self):
+        return self._spline_node
+
+    @property
+    def controls_group(self):
+        return self._controls_group
+
+    @property
+    def root_groups(self):
+        return self._root_groups
+
+    @property
+    def controls(self):
+        return self._controls
+
+    @property
+    def drivens_group(self):
+        return self._drivens_group
+
+    @property
+    def drivens(self):
+        return self._drivens
 
     def create(self):
         if not maya.cmds.pluginInfo('MayaMuscle.mll', query=True, loaded=True):
@@ -60,18 +90,18 @@ class MuscleSpline(object):
                 return False
 
         base_name = self._name
-        if not maya.cmds.objExists(self._main_set_name):
-            maya.cmds.sets(name=self._main_set_name, empty=True)
+        if not maya.cmds.objExists(self._main_muscle_set_name) and self._create_sets:
+            maya.cmds.sets(name=self._main_muscle_set_name, empty=True)
 
-        set_rig = 'set{}{}'.format(base_name, self._rig_set_suffix)
-        if not maya.cmds.objExists(set_rig):
+        set_rig = 'set{}{}'.format(base_name, self._muscle_set_suffix)
+        if not maya.cmds.objExists(set_rig) and self._create_sets:
             maya.cmds.sets(name=set_rig, empty=True)
-            maya.cmds.sets(set_rig, include=self._main_set_name)
+            maya.cmds.sets(set_rig, include=self._main_muscle_set_name)
 
-        test1 = maya.cmds.objExists('{}_{}'.format(self._muscle_spline_name, base_name))
-        test2 = maya.cmds.objExists('{}_{}'.format(self._muscle_spline_name, self._grp_suffix))
+        test1 = maya.cmds.objExists('{}_{}'.format(base_name, self._muscle_spline_name))
+        test2 = maya.cmds.objExists('{}_{}'.format(self._muscle_spline_name, self._group_suffix))
         if test1 or test2:
-            msg = 'A muscle or spline with given name "{}" already exists.' \
+            msg = 'A muscle spline with given name "{}" already exists.' \
                   '\nPlease choose a different one.'.format(self._name)
             LOGGER.warning(msg)
             qtutils.show_warning(parent=None, title='Muscle Spline already exists!', warning=msg)
@@ -79,8 +109,8 @@ class MuscleSpline(object):
 
         # create main group
         self._main_group = maya.cmds.group(
-            name='{}_{}_{}'.format(base_name, self._muscle_spline_name, self._grp_suffix), empty=True, world=True)
-        maya.cmds.sets(self._main_group, include=set_rig)
+            name='{}_{}_{}'.format(base_name, self._muscle_spline_name, self._group_suffix), empty=True, world=True)
+        self._add_to_set(set_rig, self._main_group)
 
         # create spline node
         self._spline_node = maya.cmds.createNode(
@@ -95,7 +125,7 @@ class MuscleSpline(object):
             for axis in 'xyz':
                 maya.cmds.setAttr('{}.{}{}'.format(self._spline_node_xform, xform, axis), lock=True, keyable=False)
         maya.cmds.connectAttr('time1.outTime', '{}.inTime'.format(self._spline_node), force=True)
-        maya.cmds.sets(self._spline_node, include=set_rig)
+        self._add_to_set(set_rig, self._spline_node)
 
         # make some useful attributes of cMuscleSpline node available in channel box
         for attr in ['curLen', 'pctSquash', 'pctStretch']:
@@ -108,21 +138,23 @@ class MuscleSpline(object):
         self._controls_group = maya.cmds.group(
             name='{}_{}_{}'.format(
                 base_name, self._muscle_spline_name, self._controls_group_suffix), empty=True, world=True)
-        maya.cmds.setAttr('{}.inheritsTransform'.format(self._controls_group), False)
+        # maya.cmds.setAttr('{}.inheritsTransform'.format(self._controls_group), False)
         maya.cmds.parent(self._controls_group, self._main_group)
         for xform in 'trs':
             for axis in 'xyz':
                 maya.cmds.setAttr('{}.{}{}'.format(self._controls_group, xform, axis), lock=True, keyable=False)
-        maya.cmds.sets(self._controls_group, include=set_rig)
+        self._add_to_set(set_rig, self._controls_group)
 
         # create drivens group
-        self._drivens_group = maya.cmds.group(name='{}_{}_{}'.format(base_name, self._muscle_spline_name, self._joints_group_suffix), empty=True, world=True)
+        self._drivens_group = maya.cmds.group(
+            name='{}_{}_{}'.format(base_name, self._muscle_spline_name, self._joints_group_suffix),
+            empty=True, world=True)
         maya.cmds.setAttr('{}.inheritsTransform'.format(self._drivens_group), False)
         maya.cmds.parent(self._drivens_group, self._main_group)
         for xform in 'trs':
             for axis in 'xyz':
                 maya.cmds.setAttr('{}.{}{}'.format(self._drivens_group, xform, axis), lock=True, keyable=False)
-        maya.cmds.sets(self._drivens_group, include=set_rig)
+        self._add_to_set(set_rig, self._drivens_group)
 
         # create controls
         ctrl_attrs_list = ['worldMatrix', 'tangentLength', 'jiggle', 'jiggleX', 'jiggleY', 'jiggleZ',
@@ -133,13 +165,13 @@ class MuscleSpline(object):
         self._controls = list()
         self._constraint_groups = list()
         self._root_groups = list()
-        for i in range(self._num_controls):
-            ctrl_name = '{}_{}_{}_{}'.format(base_name, self._muscle_spline_name, i, self._ctrl_suffix)
+        for i in range(self._insertion_controls):
+            ctrl_name = '{}_{}_{}_{}'.format(base_name, self._muscle_spline_name, i, self._control_suffix)
             new_ctrl = MuscleSplineControl(ctrl_name, self._control_type, self._size).create()
             root_grp = maya.cmds.group(
-                name=ctrl_name.replace(self._ctrl_suffix, self._root_suffix), empty=True, world=True)
+                name=ctrl_name.replace(self._control_suffix, self._root_group_suffix), empty=True, world=True)
             cns_grp = maya.cmds.group(
-                name=ctrl_name.replace(self._ctrl_suffix, self._auto_suffix), empty=True, world=True)
+                name=ctrl_name.replace(self._control_suffix, self._auto_group_suffix), empty=True, world=True)
             new_ctrl.root = root_grp
             new_ctrl.auto = cns_grp
 
@@ -163,7 +195,7 @@ class MuscleSpline(object):
 
             # make middle controls jiggle by default
             jiggle = 1.0
-            if i == 0 or i == (self._num_controls - 1):
+            if i == 0 or i == (self._insertion_controls - 1):
                 jiggle = 0.0
 
             maya.cmds.addAttr(
@@ -181,7 +213,7 @@ class MuscleSpline(object):
                          'jiggleImpactStart', 'jiggleImpactStop', 'cycle', 'rest']:
                 maya.cmds.setAttr('{}.{}'.format(new_ctrl.control, attr), lock=self._lock_jiggle_attributes)
 
-            if self._lock_scale:
+            if self._lock_controls_scale:
                 for xform in ['s']:
                     for axis in ['x', 'y', 'z']:
                         maya.cmds.setAttr('{}.{}{}'.format(new_ctrl.control, xform, axis), lock=True, keyable=False)
@@ -199,36 +231,35 @@ class MuscleSpline(object):
         # Also, we will aim constraints so in-between controls always aim start and end controls
         blend = ''
 
-        # if self._constrain_mid:
-        if not self._constrain_mid:
-            for i in range(1, self._num_controls - 1):
+        if self._constraint_mid_controls:
+            for i in range(1, self._insertion_controls - 1):
                 # get point constraint weight and create point constraint for intermediate controls
-                pct = 1.0 * i / (self._num_controls - 1.0)
+                pct = 1.0 * i / (self._insertion_controls - 1.0)
                 maya.cmds.pointConstraint(self._controls[0].control, self._constraint_groups[i], weight=(1.0 - pct))
                 maya.cmds.pointConstraint(
-                    self._controls[self._num_controls - 1].control, self._constraint_groups[i], weight=pct)
+                    self._controls[self._insertion_controls - 1].control, self._constraint_groups[i], weight=pct)
 
                 # create aim groups
                 # we create one for the forward aim and another one for the back aim, then we have to orient between
                 # both at the right amount
                 grp_aim_fwd = maya.cmds.group(
-                    name='{}_aimFwd_{}_{}'.format(base_name,i, self._grp_suffix), empty=True, world=True)
+                    name='{}_aimFwd_{}_{}'.format(base_name, i, self._group_suffix), empty=True, world=True)
                 grp_aim_bck = maya.cmds.group(
-                    name='{}_aimBack_{}_{}'.format(base_name, str(i), self._grp_suffix), empty=True, world=True)
+                    name='{}_aimBack_{}_{}'.format(base_name, str(i), self._group_suffix), empty=True, world=True)
                 for grp in [grp_aim_fwd, grp_aim_bck]:
                     transform_utils.match_translation(grp, self._controls[i].control)
-                maya.cmds.sets([self._root_groups[i], grp_aim_fwd, grp_aim_bck], include=set_rig)
+                self._add_to_set(set_rig, [self._root_groups[i], grp_aim_fwd, grp_aim_bck])
 
                 # aim forward group will aim the last control and aim Back group will aim to the first control this
                 # will give a twist behaviour on the aim groups
                 a_constraint = maya.cmds.aimConstraint(
-                    self._controls[self._num_controls - 1].control, grp_aim_fwd, weight=1, aimVector=(0, 1, 0),
+                    self._controls[self._insertion_controls - 1].control, grp_aim_fwd, weight=1, aimVector=(0, 1, 0),
                     upVector=(1, 0, 0), worldUpVector=(1, 0, 0), worldUpType="objectrotation",
-                    worldUpObject=self._controls[self._num_controls - 1].control)[0]
+                    worldUpObject=self._controls[self._insertion_controls - 1].control)[0]
                 b_constraint = maya.cmds.aimConstraint(
                     self._controls[0].control, grp_aim_bck, weight=1, aimVector=(0, -1, 0), upVector=(1, 0, 0),
                     worldUpVector=(1, 0, 0), worldUpType="objectrotation", worldUpObject=self._controls[0].control)[0]
-                maya.cmds.sets([a_constraint, b_constraint], include=set_rig)
+                self._add_to_set(set_rig, [a_constraint, b_constraint])
 
                 # now we drive the aims with the up info (we do this, only once) ...
                 if i == 1:
@@ -243,7 +274,7 @@ class MuscleSpline(object):
                     maya.cmds.connectAttr('{}.upAxis'.format(self._spline_node), '{}.blender'.format(blend))
                     maya.cmds.setAttr('{}.color1'.format(blend), 0, 0, 1)
                     maya.cmds.setAttr('{}.color2'.format(blend), 1, 0, 0)
-                    maya.cmds.sets(blend, include=set_rig)
+                    self._add_to_set(set_rig, blend)
 
                 # each aim constraint up vector will follow the up axis attribute of the cMuscleSpineNode
                 # we can switch between Z or X up axis if you get flipping when rotating controls
@@ -255,11 +286,11 @@ class MuscleSpline(object):
                 # position of its respective control)
                 p_cons_fwd1 = maya.cmds.pointConstraint(self._controls[0].control, grp_aim_fwd, weight=(1.0 - pct))[0]
                 p_cons_fwd2 = maya.cmds.pointConstraint(
-                    self._controls[self._num_controls - 1].control, grp_aim_fwd, weight=pct)[0]
+                    self._controls[self._insertion_controls - 1].control, grp_aim_fwd, weight=pct)[0]
                 p_cons_back1 = maya.cmds.pointConstraint(self._controls[0].control, grp_aim_bck, weight=(1.0 - pct))[0]
                 p_cons_back2 = maya.cmds.pointConstraint(
-                    self._controls[self._num_controls - 1].control, grp_aim_bck, weight=pct)[0]
-                maya.cmds.sets([p_cons_fwd1, p_cons_fwd2, p_cons_back1, p_cons_back2], include=set_rig)
+                    self._controls[self._insertion_controls - 1].control, grp_aim_bck, weight=pct)[0]
+                self._add_to_set(set_rig, [p_cons_fwd1, p_cons_fwd2, p_cons_back1, p_cons_back2])
 
                 # auto groups will follow the orientation of the aim groups. So, the controls (which are child of
                 # the auto groups) will follow the aim orientation
@@ -267,18 +298,18 @@ class MuscleSpline(object):
                 maya.cmds.setAttr('{}.interpType'.format(o_cons_bck), 2)
                 o_cons_fwd = maya.cmds.orientConstraint(grp_aim_fwd, self._constraint_groups[i], weight=pct)[0]
                 maya.cmds.setAttr('{}.interpType'.format(o_cons_fwd), 2)
-                maya.cmds.sets([o_cons_bck, o_cons_fwd], include=set_rig)
+                self._add_to_set(set_rig, [o_cons_bck, o_cons_fwd])
 
                 # create root grops for each one of the aim groups (so its xfrorms are zeroed out)
                 grp_aim_fwd_root = maya.cmds.group(
-                    name='{}_{}_grpAimFwd_{}'.format(base_name, self._muscle_spline_name, self._root_suffix),
+                    name='{}_{}_grpAimFwd_{}'.format(base_name, self._muscle_spline_name, self._root_group_suffix),
                     empty=True, world=True)
                 grp_aim_bck_root = maya.cmds.group(
-                    name='{}_{}_grpAimBck_{}'.format(base_name, self._muscle_spline_name, self._root_suffix),
+                    name='{}_{}_grpAimBck_{}'.format(base_name, self._muscle_spline_name, self._root_group_suffix),
                     empty=True, world=True)
                 transform_utils.match_translation(grp_aim_fwd_root, grp_aim_fwd)
                 transform_utils.match_translation(grp_aim_bck_root, grp_aim_bck)
-                maya.cmds.sets([grp_aim_fwd_root, grp_aim_bck_root], include=set_rig)
+                self._add_to_set(set_rig, [grp_aim_fwd_root, grp_aim_bck_root])
 
                 maya.cmds.parent(grp_aim_fwd_root, self._root_groups[i])
                 maya.cmds.parent(grp_aim_fwd, grp_aim_fwd_root)
@@ -286,9 +317,9 @@ class MuscleSpline(object):
                 maya.cmds.parent(grp_aim_bck, grp_aim_fwd_root)
 
         self._drivens = list()
-        for i in range(self._num_drivens):
+        for i in range(self._driven_joints):
             # get normalized values between 0 and 1 and the correct name
-            u = i / (self._num_drivens - 1.0)
+            u = i / (self._driven_joints - 1.0)
             name = '{}_{}_{}_{}'.format(base_name, self._muscle_spline_name, i, self._driven_suffix)
             if self._driven_type == "joint":
                 self._drivens.append(maya.cmds.joint(name=name))
@@ -303,7 +334,7 @@ class MuscleSpline(object):
             maya.cmds.select(clear=True)
             maya.cmds.addAttr(self._drivens[i], longName="uValue", minValue=0.0, maxValue=1.0, defaultValue=u, k=True)
             maya.cmds.parent(self._drivens[i], self._drivens_group)
-            maya.cmds.sets(self._drivens[i], include=set_rig)
+            self._add_to_set(set_rig, self._drivens[i])
             maya.cmds.connectAttr(
                 '{}.uValue'.format(self._drivens[i]),
                 '{}.readData[{}].readU'.format(self._spline_node, i), force=True)
@@ -322,9 +353,21 @@ class MuscleSpline(object):
         maya.cmds.setAttr('{}.lenSquash'.format(self._spline_node), spline_length * 0.5)
         maya.cmds.setAttr('{}.lenStretch'.format(self._spline_node), spline_length * 2.0)
 
+        # TODO: If lock control scale is not enabled we should automatically sclae constraint drivens to controls
+        # TODO: We should handle all scenarios (same amount of both,
+        #  more controls than drivens and more drivens than controls)
+        # if not self._lock_controls_scale:
+        #     for ctrl, jnt in zip(self._controls, self._drivens):
+
         maya.cmds.select(self._main_group)
 
         return self._spline_node
+
+    def _add_to_set(self, set_name, objects_to_add):
+        if not self._create_sets:
+            return
+
+        maya.cmds.sets(objects_to_add, include=set_name)
 
 
 class MuscleSplineControl(object):
@@ -388,3 +431,17 @@ class MuscleSplineControl(object):
             self._ctrl = maya.cmds.group(name=self._name, empty=True, world=True)
 
         return self
+
+    def zero_out_jiggle_attributes(self):
+        """
+        Zero out all jiggle attributes of the muscleSpline control
+        """
+
+        for attr in ['jiggle', 'jiggleX', 'jiggleX', 'jiggleY', 'jiggleZ', 'jiggleImpact']:
+            maya.cmds.setAttr('{}.{}'.format(self._ctrl, attr), 0.0)
+
+    def lock_and_hide_jiggle_attributes(self):
+        for attr in ['tangentLength', 'jiggle', 'jiggleX', 'jiggleX', 'jiggleY', 'jiggleZ', 'jiggleImpact',
+                     'jiggleImpactStart', 'jiggleImpactStop', 'cycle', 'rest']:
+            maya.cmds.setAttr('{}.{}'.format(self._ctrl, attr), lock=True, keyable=False)
+            maya.cmds.setAttr('{}.{}'.format(self._ctrl, attr), channelBox=False)
